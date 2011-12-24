05bd2b0ebc42197ebd9e1d39df1bb967065de14b
François Le Lay <mfworx@gmail.com>
2011-12-24 10:43:28 +0100

    added noir dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject wareop "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Plug and play operational datawarehouse"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [noir "1.2.1"]])
