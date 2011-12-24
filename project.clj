536b88e0cfdb620523b3edf757d5169ab2dc046b
François Le Lay <mfworx@gmail.com>
2011-12-24 10:47:46 +0100

    changed deps from clojure 1.2.1 to 1.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject wareop "1.0.0-SNAPSHOT"
   :description "Plug and play operational datawarehouse"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
 		 [noir "1.2.1"]])
